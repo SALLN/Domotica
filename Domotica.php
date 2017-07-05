6598e6a4710610dbe0d5341ba1dd5b5b5197e66a
SALLN <stevenllerena@hotmail.com>
2017-07-05 18:37:28 -0500

    1.0

---
+++ Domotica.php
@@ -0,0 +1,22 @@
+<?php
+// session_start();
+// include("ConexionMySQL.php");
+//
+// $consulta=mysql_query("INSERT INTO  Formulas (COEFICIENTES) VALUES (3)");
+//
+// mysql_free_result($consulta);
+// mysql_close($conexion);
+
+/* Abrir un socket al puerto 1234 en localhost */
+$socket = stream_socket_client('tcp://ticollcloud.ddns.net:55056');
+
+/* Enviar información ordinaria mediante canales oridinarios. */
+fwrite($socket, "Transmisión de información normal.");
+
+/* Enviar más información fuera de banda. */
+stream_socket_sendto($socket, "", STREAM_OOB);
+
+
+// fclose($socket);
+
+?>
