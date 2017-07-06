0e5b7b6e819c4dd272685f54da789aa61bc65f83
SALLN <stevenllerena@hotmail.com>
2017-07-05 23:14:11 -0500

    s

--- BorrarDisp.php
+++ BorrarDisp.php
@@ -3,7 +3,7 @@ include("ConexionMySQL.php");
 
 $NombreTabla="Usuario";
 $PuertoDisp=4;
-mysqli_query($conexion,"UPDATE $NombreTabla SET NOMBRE = '', ASIGNADO = 'NO' WHERE $NombreTabla.ID = $PuertoDisp");
+mysqli_query($conexion,"UPDATE $NombreTabla SET  ASIGNADO = 'NO' WHERE $NombreTabla.ID = $PuertoDisp");
 
 mysqli_close($conexion);
 ?>
