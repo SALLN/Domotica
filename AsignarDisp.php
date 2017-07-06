7d28ce108df0ee8ece36a89a2b10e2b37c169887
SALLN <stevenllerena@hotmail.com>
2017-07-05 22:39:34 -0500

    ss

--- AsignarDisp.php
+++ AsignarDisp.php
@@ -1,6 +1,7 @@
 <?php
 include("ConexionMySQL.php");
 
+$NombreTabla="Usuario";
 $PuertoDisp=4;
 $NomDisp="Primer Dispositivo";
 mysqli_query($conexion,"UPDATE $NombreTabla` SET NOMBRE = $NomDisp, ASIGNADO = 'OK', ESTADO = '0' WHERE $NombreTabla.ID = $PuertoDisp");
