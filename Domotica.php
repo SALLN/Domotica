c628af0dac45a4ce0733cd55a35021bda749a8c1
SALLN <stevenllerena@hotmail.com>
2017-07-05 19:02:31 -0500

    s

--- Domotica.php
+++ Domotica.php
@@ -7,7 +7,7 @@
 // mysql_free_result($consulta);
 // mysql_close($conexion);
 
-/* Abrir un soocket al puerto 1234 en localhost */
+/* Abrir un socket al puerto 1234 en localhost */
 $socket = stream_socket_client('tcp://ticollcloud.ddns.net:55056');
 
 /* Enviar información ordinaria mediante canales oridinarios. */
