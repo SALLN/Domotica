bc44c685fb34a432da56aea3d0377d7a1387e403
SALLN <stevenllerena@hotmail.com>
2017-07-05 23:25:04 -0500

    s

--- AsignarDisp.php
+++ AsignarDisp.php
@@ -1,7 +1,7 @@
 <?php
 include("ConexionMySQL.php");
 
-$NombreTabla=$_POST['NombreTabla'];
+$NombreTabla=$_POST['Usuario'];
 $PuertoDisp=$_POST['PuertoDisp'];
 $NombreDisp=$_POST['NombreDisp'];
 mysqli_query($conexion,"UPDATE $NombreTabla SET NOMBRE = '$NombreDisp', ASIGNADO = 'OK', ESTADO = '0' WHERE $NombreTabla.ID = $PuertoDisp");
