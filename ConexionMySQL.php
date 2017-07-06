e66f88d16885ff6a647c519d720d3434dc1bd369
SALLN <stevenllerena@hotmail.com>
2017-07-05 19:01:05 -0500

    s

--- ConexionMySQL.php
+++ ConexionMySQL.php
@@ -1,4 +1,4 @@
 <?php
 	$conexion = @mysqli_connect("localhost","root","steven1509","Domotica") or die("No se encontró el servidor");
-	//mysql_select_db("diseno",$conexion)or die("No se encontró la base de datos");
+	//mysql_sedlect_db("diseno",$conexion)or die("No se encontró la base de datos");
 ?>
