33bcae557f7793d5f856660e830a8132634a48f2
SALLN <stevenllerena@hotmail.com>
2017-07-05 23:48:07 -0500

    s

--- ActualizarDisp.php
+++ ActualizarDisp.php
@@ -3,6 +3,7 @@ include("ConexionMySQL.php");
 
 $EstadoDisp=$_POST['Estado'];
 $PuertoDisp=$_POST['PuertoDisp'];
+$NombreTabla=$_POST['Usuario'];
 
 mysqli_query($conexion,"UPDATE $NombreTabla SET ESTADO = '$EstadoDisp' WHERE $NombreTabla.ID = $PuertoDisp");
 mysqli_close($conexion);
