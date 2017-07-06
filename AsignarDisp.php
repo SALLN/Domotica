e03793121316ed43823d4ecd8857cdbab68852b5
SALLN <stevenllerena@hotmail.com>
2017-07-05 22:50:25 -0500

    s

--- AsignarDisp.php
+++ AsignarDisp.php
@@ -4,7 +4,7 @@ include("ConexionMySQL.php");
 $NombreTabla="Usuario";
 $PuertoDisp=4;
 $NomDisp="Primer Dispositivo";
-mysqli_query($conexion,"UPDATE $NombreTabla` SET NOMBRE = $NomDisp, ASIGNADO = 'OK', ESTADO = '0' WHERE $NombreTabla.ID = $PuertoDisp");
+mysqli_query($conexion,"UPDATE $NombreTabla SET NOMBRE = $NomDisp, ASIGNADO = 'OK', ESTADO = '0' WHERE $NombreTabla.ID = $PuertoDisp");
 
 
 //mysqli_free_result($consulta);
--- CrearTabla.php
+++ CrearTabla.php
@@ -3,7 +3,7 @@ include("ConexionMySQL.php");
 
 $NombreTabla="Usuario";
 $NumDisp=4;
-$consulta=mysqli_query($conexion,"CREATE TABLE `Domotica`.$NombreTabla ( `ID` INT(10) NOT NULL AUTO_INCREMENT , `NOMBRE` VARCHAR(10) NOT NULL , `ASIGNADO` VARCHAR(10) NOT NULL , `ESTADO` INT(1) NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB") or die("Problemas en consulta: ");
+$consulta=mysqli_query($conexion,"CREATE TABLE `Domotica`.$NombreTabla ( `ID` INT(10) NOT NULL AUTO_INCREMENT , `NOMBRE` VARCHAR(100) NOT NULL , `ASIGNADO` VARCHAR(10) NOT NULL , `ESTADO` INT(1) NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB") or die("Problemas en consulta: ");
 for ($i = 1; $i <= $NumDisp; $i++) {
 mysqli_query($conexion,"INSERT INTO $NombreTabla (NOMBRE,ASIGNADO,ESTADO) VALUES('','NO',0)");
 }
