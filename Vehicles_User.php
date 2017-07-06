fc23cdf9b42b92d02eb1d55acfdcce7d35d0fcf8
SALLN <stevenllerena@hotmail.com>
2017-07-05 19:23:58 -0500

    s

--- Vehicles_User.php
+++ Vehicles_User.php
@@ -2,7 +2,8 @@
 session_start();
 include("ConexionMySQL.php");
 
-$consulta=mysqli_query($conexion,"CREATE TABLE `Domotica`.`usuario` ( `ID` INT(10) NOT NULL AUTO_INCREMENT , `NOMBRE` VARCHAR(10) NOT NULL , `ASIGNADO` VARCHAR(10) NOT NULL , `ESTADO` INT(1) NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB") or die("Problemas en consulta: ");
+$NombreTabla="Usuario"
+$consulta=mysqli_query($conexion,"CREATE TABLE `Domotica`.$NombreTabla ( `ID` INT(10) NOT NULL AUTO_INCREMENT , `NOMBRE` VARCHAR(10) NOT NULL , `ASIGNADO` VARCHAR(10) NOT NULL , `ESTADO` INT(1) NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB") or die("Problemas en consulta: ");
 
 mysqli_free_result($consulta);
 mysqli_close($conexion);
