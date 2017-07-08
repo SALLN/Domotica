7c07aaa3a0c977b4540c7dbee38e23c6ea535ca9
SALLN <stevenllerena@hotmail.com>
2017-07-08 12:10:46 -0500

    s

--- AsignarDisp.php
+++ AsignarDisp.php
@@ -1,9 +1,12 @@
 <?php
 include("ConexionMySQL.php");
 
-$NombreTabla=$_POST['Usuario'];
-$PuertoDisp=$_POST['PuertoDisp'];
-$NombreDisp=$_POST['NombreDisp'];
-mysqli_query($conexion,"UPDATE $NombreTabla SET NOMBRE = '$NombreDisp', ASIGNADO = 'OK', ESTADO = '0' WHERE $NombreTabla.ID = $PuertoDisp");
+//$NombreTabla=$_POST['Usuario'];
+//$PuertoDisp=$_POST['PuertoDisp'];
+//$NombreDisp=$_POST['NombreDisp'];
+$NombreTabla="Usuario";
+$PuertoDisp=4;
+$NombreDisp="Lo que sea";
+mysqli_query($conexion,"UPDATE $NombreTabla SET NOMBRE = '$NombreDisp', ASIGNADO = 'OK', ESTADO = '0' WHERE $NombreTabla.ID = $PuertoDisp") or die("Problemas en consulta: ".mysqli_error($conexion));
 mysqli_close($conexion);
 ?>
